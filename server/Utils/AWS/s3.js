a78adb721431402cbb6310a91517381ef9045c0a
RVS DINESH <57357275+rvsdinesh@users.noreply.github.com>
2021-10-20 16:23:49 +0530

    Update s3.js

--- server/Utils/AWS/s3.js
+++ server/Utils/AWS/s3.js
@@ -1,8 +1,8 @@
 import AWS from "aws-sdk";
 
 const s3Bucket = new AWS.S3({
-  accessKeyId: "AKIAR6Y3U7NXRUW3WU67",
-  secretAccessKey: "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",
+  accessKeyId: "",
+  secretAccessKey: "",
   region: "ap-south-1"
 });
 
